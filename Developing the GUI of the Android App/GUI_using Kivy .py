c36f7aa1a717c6e9f79994d9947aa7646d0edffb
Wafaa_Siddig <116094810+Wafaa916@users.noreply.github.com>
2022-12-16 10:35:47 +0200

    Update GUI_using Kivy .py

--- Developing the GUI of the Android App/GUI_using Kivy .py
+++ Developing the GUI of the Android App/GUI_using Kivy .py
@@ -7,7 +7,7 @@ from kivy.uix.image import Image
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 
-# Import other kivy stuff
+
 from kivy.clock import Clock
 from kivy.core.window import Window
 from kivy.graphics.texture import Texture
